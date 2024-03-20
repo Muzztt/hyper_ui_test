47d6a52bb4f8e2468a7b60112480fc8082cd148b
mustofaakbar <115098213+Muzztt@users.noreply.github.com>
2024-03-20 17:14:08 +0700

    update

--- lib/core.dart
+++ lib/core.dart
@@ -8,6 +8,8 @@ export 'package:hyper_ui_test_test/core_package.dart';
 export 'package:hyper_ui_test_test/debug.dart';
 export 'package:hyper_ui_test_test/module/dashboard/controller/dashboard_controller.dart';
 export 'package:hyper_ui_test_test/module/dashboard/view/dashboard_view.dart';
+export 'package:hyper_ui_test_test/module/main_navigation/controller/main_navigation_controller.dart';
+export 'package:hyper_ui_test_test/module/main_navigation/view/main_navigation_view.dart';
 export 'package:hyper_ui_test_test/setup.dart';
 export 'package:hyper_ui_test_test/shared/theme/theme.dart';
 export 'package:hyper_ui_test_test/shared/theme/theme_config.dart';
---
+++ lib/module/main_navigation/controller/main_navigation_controller.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hyper_ui_test_test/core.dart';
+import '../view/main_navigation_view.dart';
+
+class MainNavigationController extends State<MainNavigationView> {
+  static late MainNavigationController instance;
+  late MainNavigationView view;
+
+  @override
+  void initState() {
+    instance = this;
+    super.initState();
+  }
+
+  @override
+  void dispose() => super.dispose();
+
+  @override
+  Widget build(BuildContext context) => widget.build(context, this);
+}
